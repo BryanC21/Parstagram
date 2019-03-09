8977fbe29af3cece38beddcef1df616ebccff333
Bryan Caldera <bryanicaldera@gmail.com>
2019-03-08 23:35:28 -0800

    Initial Commit

---
+++ Parstagram/LogInViewController.swift
@@ -0,0 +1,61 @@
+//
+//  LogInViewController.swift
+//  Parstagram
+//
+//  Created by Bryan Caldera on 3/8/19.
+//  Copyright © 2019 BryanCaldera. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = userNameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "logInSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription ?? "Something went Wrong")")
+            }
+        }
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = userNameField.text
+        let password = passwordField.text
+        
+        PFUser.logInWithUsername(inBackground: username!, password: password!) { (user, error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "logInSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription ?? "Something went wrong")")
+            }
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
